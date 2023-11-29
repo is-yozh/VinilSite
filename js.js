ea488fb863eeeab1af8eb6555ec872955b3bcc6e
is-yozh <vthen3897@gmail.com>
2023-11-29 11:05:23 +0100

    On branch main  Your branch is up to date with 'origin/main'.

---
+++ js.js
@@ -0,0 +1,25 @@
+let productsGrid = document.getElementById('products-grid');
+let productsAray = [];
+let xhr = new XMLHttpRequest();
+let url = 'https://my-json-server.typicode.com/Dmitry-Krtadze/Vinil';
+
+xhr.open('GET', url + '/products');
+xhr.responseType = 'json';
+xhr.onload = function(){
+    let products = xhr.response;
+    productsGrid.innerHTML = null;
+    products.forEach(p => {
+        productsAray.push(p);
+        let pElm = document.createElement('div');
+        pElm.classList.add('product');
+        pElm.innerHTML = `
+            <h2 class='product-name'>${p.name}</h2>
+            <img class='product-photo' src='${p.photo_url}' alt='${p.name}'>
+            <p class='product-price'><b>Price: </b>${p.description}</p>
+            <a href=''>Seller profile</a>
+            <button>Buy</button>
+        `;
+        productsGrid.append(pElm);
+    });
+}
+xhr.send();
