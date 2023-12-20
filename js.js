02953ad1e212e7bc2d12cac128ca1e3216fb2e9f
is-yozh <vthen3897@gmail.com>
2023-12-20 11:09:35 +0100

    modified:   user.js 	modified:   userPage.html

--- js.js
+++ js.js
@@ -5,8 +5,10 @@ let url = 'https://my-json-server.typicode.com/Dmitry-Krtadze/Vinil';
 
 xhr.open('GET', url + '/products');
 xhr.responseType = 'json';
+
 xhr.onload = function(){
     let products = xhr.response;
+    productAray = xhr.response;
     productsGrid.innerHTML = null;
     products.forEach(p => {
         productsAray.push(p);
@@ -18,9 +20,58 @@ xhr.onload = function(){
             <p class='product-price'><b>Price: </b>${p.price}</p>
             <p class='product-price'><b>Description: </b>${p.description}</p>
             <a href='userPage.html?id=${p.author_id}'>Seller profile</a>
-            <button>Buy</button>
+            <button onclick="addProductToCart(${p.id});">Buy</button>
         `;
         productsGrid.append(pElm);
     });
 }
 xhr.send();
+
+let cardProd = document.getElementById('cart-products');
+let cart = [];
+
+if(localStorage.getItem('cart'))
+{
+    cart = JSON.parse(localStorage.getItem('cart'));
+    drawCartProducts();
+}
+
+function addProductToCart(id){
+    let product = productsAray.find(function(p){
+        return p.id == id;
+    })
+    cart.push(product);
+    drawCartProducts();
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function drawCartProducts(){
+    if(cart.length === 0) return cardProd.innerHTML = 'Cart is empty';
+    cardProd.innerHTML = null;
+    let sum = 0;
+    cart.forEach(function(p){
+        cardProd.innerHTML += `
+            <p><img src="${p.photo_url}">
+                ${p.name} | ${p.price} $
+            </p>
+            <hr>
+
+        `;
+        sum += p.price;
+    });
+    cardProd.innerHTML += `
+        <b>Total Price: ${sum} $</b>
+        <button onclick="buyAll();">Buy All</button>
+    `;
+    localStorage.setItem('cart', '[]');
+}
+
+function buyAll(){
+    cart = [];
+    cardProd.innerHTML = 'Money was withdraw from your credit card';
+}
+
+
+function openCart(){
+    cardProd.classList.toggle('hide');
+}
--- user.js
+++ user.js
@@ -7,15 +7,44 @@ let url = 'https://my-json-server.typicode.com/Dmitry-Krtadze/Vinil';
 let urlRequest = new XMLHttpRequest();
 
 userRequest = new XMLHttpRequest();
-userRequest.open('GET', `${url}/users/${id}`);
+userRequest.open('GET',`${url}/users/${id}`);
 userRequest.responseType = 'json';
 userRequest.onload = function(){
-    let user = userRequest.responseType;
+    let user = userRequest.response;
     profile.innerHTML = `
         <h1>${user.name}</h1>
         <h2>${user.sirname}</h2>
         <img class="profile-img" src="${user.photo_url}">
-        <p>Balanse: ${user.balance}</p>
+        <p>Balanse: ${user.balance}</p> 
     `
 }
 userRequest.send();
+
+
+
+let productsGrid = document.getElementById('user-products-grid');
+let productRequest = new XMLHttpRequest();
+
+productRequest.open('GET',`${url}/products?author_id=${id}`);
+productRequest.responseType = 'json';
+productRequest.onload = function(){
+    let products = productRequest.response;
+    productsGrid.innerHTML = null;
+    products.forEach(p => {
+        productsGrid.innerHTML += `
+        <div class="product">
+            <h2 class="product-name">${p.name}</h2>
+            <img class='product-photo' src='${p.photo_url}' alt='${p.name}'>
+            <p class='product-price'>
+                <b>Price: </b>${p.price}
+            </p>
+            <p class='product-description'>
+                <b>Description: </b>${p.description}
+            </p>
+        
+            </div>
+            
+        `;
+    });
+}
+productRequest.send();
